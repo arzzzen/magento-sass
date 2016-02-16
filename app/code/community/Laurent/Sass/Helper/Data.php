05ced626840947670d7259642111dd0abfb84e98
arzzzen <arzenslim@gmail.com>
2016-02-16 14:20:05 +0300

    Remove unnecessary require

--- app/code/community/Laurent/Sass/Helper/Data.php
+++ app/code/community/Laurent/Sass/Helper/Data.php
@@ -11,8 +11,6 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-require_once 'scssphp/scss.inc.php';
-
 class Laurent_Sass_Helper_Data extends Mage_Core_Helper_Abstract
 {
     /**
